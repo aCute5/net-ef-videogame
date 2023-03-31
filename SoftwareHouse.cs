60e0602566714ad37aee111bd23c899aa0c4fea3
Dario Costantini <costantini95dario@gmail.com>
2023-03-31 15:38:46 +0200

    creazione class

---
+++ SoftwareHouse.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace net_ef_videogame
+{
+    [Table("softwarehouses")]
+    [Index(nameof(SoftwareHouse_id), IsUnique =true)]
+   public class SoftwareHouse
+    {
+        public int SoftwareHouse_id { get; set; }
+        public string? Name { get; set; }
+
+        public long  IVA { get; set; }
+
+        public string? City { get; set; }
+
+        public string? Country { get; set; }
+
+        public List<Videogame>? Videogames { get; set; }
+    }
+}
---
+++ Videogame.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_ef_videogame
+{
+    [Table("Videogame")]
+    [Index(nameof(VideogameId), IsUnique = true)]
+    public class Videogame
+    {
+        public int VideogameId { get; set; }
+        public string? Name { get; set; }
+        public string? Overview { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public int SoftwareHouse_id { get; set; }
+        public SoftwareHouse? SoftwareHouse { get; set; }
+
+
+    }
+}
